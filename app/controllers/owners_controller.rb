b94c7c27f53b87fc461b061b430de872655b4c30
Adam Urbina <adamurbina@gmail.com>
2018-08-31 18:32:45 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,6 +12,8 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     binding.pry
+    @owner = Owner.create(params[:owner])
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
